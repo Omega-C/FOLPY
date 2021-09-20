354de629e8a32f63e1f0db0bb01d59275cdb6c7b
Omega-C <42104762+Omega-C@users.noreply.github.com>
2021-09-20 07:30:14 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,8 @@
+from functools import lru_cache
+
 StatementParenthesis=True
 
+_butIn=lambda l,item:[q for q in l if q!=item]
 _condenseIter=lambda x:([x] if not hasattr(x,"__iter__") else x)
 _flatten=lambda *x:[e for a in x for e in (_flatten(*a) if hasattr(a,"__iter__") else (a,))]
 
@@ -36,6 +39,19 @@ class Operator:
 	def __repr__(self):
 		return(self.name)
 
+	def __eq__(self,other):
+		if type(other)==AOperator:return(other.__eq__(self))
+		return(self.name==other.name)
+
+class AOperator(Operator):
+	def __init__(self,*args,exclusion=[],**kwargs):
+		super().__init__(self,*args,**kwargs)
+		self.exclusion=exclusion
+
+	def __eq__(self,other):
+		if type(other)!=type(self):return(other not in self.exclusion)
+		return(self.name==other.name)
+
 class Variable:
 	def __init__(self,name):
 		self.name=name
@@ -45,12 +61,16 @@ class Variable:
 
 class Statement:
 	def __init__(self,var,name):
-		self.parents=[None]
 		self.operator=Operators.NOPERATOR
 		self.variable=var
 		self.name=name
 		self.statements=[self]
+		self._init()
+
+	def _init(self):
+		self.parents=[None]
 		self.parentRule=None
+		self.symetry=False
 	
 	@property
 	def ruleParameters(self):
@@ -64,16 +84,26 @@ class Statement:
 	def setVariable(self,newVar):
 		self.variable=newVar
 
+	lru_cache(maxsize=256)
 	def returnRules(self,pool):
 		return([item for lis in [v for v in [r(self,pool) for r in self.operator.rules] if v!=None] for item in _condenseIter(lis)])
 	
+	def __hash__(self):
+		return(hash((self.name,id(self.variable))))
+
 	def __repr__(self):
 		if StatementParenthesis:
 			return(f"{self.name}({self.variable})")
 		return(f"{self.name}{self.variable}")
 	
+	def compareSym(self,other):
+		return(((self.statements==other.statements)|(self.statements==other.statements[::-1]))&(other.operator==self.operator))
+
 	def __eq__(self,other):
+		if type(other)==AStatement:return(other.__eq__(self))
 		if type(other)!=type(self):return(False)
+		if other.symetry and not self.symetry:return(other.compareSym(self))
+		if self.symetry:return(self.compareSym(other))
 		if self.variable==other.variable and self.name==other.name:return(True)
 		return(False)
 
@@ -85,6 +115,10 @@ class Statement:
 		if self.operator==Operators.NOT:return(self.statements[0])
 		return(StatementFunc(self,Operators.NOT))
 	
+	def __mul__(self,other):
+		if not isinstance(other,Statement):raise(TypeError(f"Unsupported type {type(other)}"))
+		return(StatementFunc([self,other],Operators.ABSENT))
+
 	def __or__(self,other):
 		if not isinstance(other,Statement):raise(TypeError(f"Unsupported type {type(other)}"))
 		return(StatementFunc([self,other],Operators.OR))
@@ -95,7 +129,7 @@ class Statement:
 
 	def __floordiv__(self,other):
 		if not isinstance(other,Statement):raise(TypeError(f"Unsupported type {type(other)}"))
-		return(StatementFunc([self,other],Operators.EQUIVILANCE))
+		return(StatementFunc([self,other],Operators.EQUIVILENCE))
 
 	def _copy(self):
 		nw=self.__new__(self.__class__)
@@ -108,21 +142,47 @@ class Statement:
 		nw.parents=parents
 		return(nw)
 
+	@property
+	def sym(self):
+		nw=self._copy()
+		nw.symetry=True
+		return(nw)
+
 	def __mod__(self,rule):
 		nw=self._copy()
 		nw.parentRule=rule
 		return(nw)
 
+class AStatement(Statement):
+	def __init__(self,u,o=None):
+		self.ident=u
+		self.o=o!=None
+		self.operator=(o if self.o else None)
+
+	def __eq__(self,other):
+		if type(other)==AStatement and o:return((self.ident==other.ident)&(self.operator==other.operator))
+		if type(other)==AStatement:return(self.ident==other.ident)
+		return(True)
+
+	def __hash__(self):
+		return(hash((self.ident,self.operator)))
+
+	def __repr__(self):
+		return(f"{self.ident}")
+
 class StatementFunc(Statement):
 	def __init__(self,statements,operator):
 		self.statements=_condenseIter(statements)
 		self.operator=operator
-		self.parents=[None]
-		self.parentRule=None
+		super()._init()
 	
 	def __eq__(self,other):
+		if type(other)==AStatement:return(other.__eq__(self))
 		if type(other)!=type(self):return(False)
-		if self.operator==other.operator and self.statements==other.statements:return(True)
+		if other.symetry and not self.symetry:return(other.compareSym(self))
+		if self.symetry:return(self.compareSym(other))
+		if self.operator==other.operator and other.statements==self.statements:return(True)
+		return(False)
 
 	def __repr__(self):
 		ret=[]
@@ -134,12 +194,22 @@ class StatementFunc(Statement):
 			ret.append(repr(s))
 		return(self.operator.makeSyntax(ret))
 
+	def __hash__(self):
+		return(hash((self.conclusion,hash(self.statements))))
+
 class Pool:
-	def __init__(self,propositions):
+	def __init__(self,propositions,conclusion):
+		self.conclusion=conclusion
 		self.propositions=propositions
 		self.pool=list(propositions)
 		self.initialPool=list(propositions)
 	
+	def __hash__(self):
+		return(hash((self.conclusion,len(self.pool))))
+
+	def setConclusion(self,newConclusion):
+		self.conclusion=newConclusion
+
 	def append(self,newObject):
 		if not (newObject in self.pool):
 			self.pool.append(newObject)
@@ -149,9 +219,9 @@ class Pool:
 		for st in statement.returnRules(self):
 			self.append(st)
 
-	def checkForConclusion(self,conclusion):
+	def checkForConclusion(self):
 		for ob in self.pool:
-			if ob==conclusion:
+			if ob==self.conclusion:
 				return(ob)
 		return(None)
 
@@ -186,28 +256,44 @@ class Pool:
 				return(ob)
 		return(None)
 	
-	def getValueMatch(self,matchLambda):
+	def getValueMatch(self,matchLambda,conc=False,er=False):
 		ret=[]
-		for ob in self.pool:
+		errs=[]
+		for ob in [*self.pool,*([self.conclusion] if conc else [])]:
 			try:
 				if matchLambda(ob):
 					ret.append(ob)
-			except Exception as e:pass
+			except Exception as e:errs.append(e)
+		if er:
+			return(ret,errs)
 		return(ret)
 
-	def proof(self,conclusion):
-		concluded=self.checkForConclusion(conclusion)
+	def proof(self):
+		concluded=self.checkForConclusion()
 		if concluded==None:
 			return(None)
-		proof=list(enumerate(self.initialPool))
+		proof=list(zip([i for i in range(1,len(self.initialPool)+1)],self.initialPool,[None for i in range(len(self.initialPool))]))
+		numberer=lambda x:[p[0] for p in proof if p[1] in x.parents]
 		paren=lambda x:_flatten([[*paren(b),b] if b.parents!=[None] else b for b in x.parents])
-		steps=[p for p in paren(concluded) if p not in self.initialPool][::-1]
-		return(proof+steps)
+		steps=[p for p in paren(concluded) if p not in self.initialPool]+[concluded]
+		for n,h in enumerate(steps):
+			proof.append((n+len(self.initialPool)+1,h,(numberer(h),h.parentRule)))
+		return(proof)
 
-class Inspecific:
+	@property
+	def contradictions(self):
+		contradictions=[]
+		for item in self.pool:
+			if (~item in self.pool) and not ([~item,item] in contradictions):
+				contradictions.append([item,~item])
+		return(contradictions)
+
+class Insp:
 	X=Variable(name="x")
 	Y=Variable(name="y")
 	Z=Variable(name="z")
+	Xs=AStatement("X")
+	Ys=AStatement("Y")
 
 class Rules:
 	_impl=lambda st,op,full,p:((~st[0])|st[1])^full
@@ -217,8 +303,21 @@ class Rules:
 	_ds=lambda st,op,full,p:st[1]^[full,p.getValue(~st[0])]
 	_mp=lambda st,op,full,p:st[1]^[full,p.getValue(st[0])]
 	_hs=lambda st,op,full,p:[(st[0]>q.statements[1])^[full,q] for q in p.getValueMatch(lambda x:(x.statements[0]==st[1])&(x.operator==Operators.IMPLICATION))]
+	_dm=lambda st,op,full,p:(~StatementFunc([~st[0],~st[1]],(Operators.AND if op==Operators.OR else Operators.OR)))^full
+	_rdm=lambda st,op,full,p:(StatementFunc([~st[0].statements[0],~st[0].statements[1]],(Operators.AND if op==Operators.OR else Operators.OR)))^full
+	_simp=lambda st,op,full,p:[st[0]^full,st[1]^full]
+	_addOrM=lambda st,p:lambda match:((~(Insp.Xs|st).sym)|Insp.Xs) in [*match.returnRules(p),match]#ITS THE REUTRN RULES
+	_addImplM=lambda st,p:lambda match:((~(Insp.Xs>st))|Insp.Xs) in [*match.returnRules(p),match]
+	_addOr=lambda st,op,full,p:[((full|q)^full) for q in p.getValueMatch(Rules._addOrM(full,p))]
+	_addImpl=lambda st,op,full,p:[((full|(~q))^full) for q in p.getValueMatch(Rules._addImplM(full,p))]
+	_add=lambda st,op,full,p:Rules._addImpl(st,op,full,p)+Rules._addOr(st,op,full,p)
+	#match if ~(a|b)|x in return rules or is object
 
 	HS=Rule(_hs,name="Hypothetical Syllogism")
+	ADD=Rule(_add,name="Addition")
+	SIMP=Rule(_simp,name="Simplification")
+	DM=Rule(_dm,name="De Morgan's Rule")
+	RDM=Rule(_rdm,name="De Morgan's Rule")
 	DS=Rule(_ds,conditions=[lambda st,p:p.getValue(~st.statements[0])],name="Disjunctive Syllogism")
 	MP=Rule(_mp,conditions=[lambda st,p:p.getValue(st.statements[0])],name="Modus Ponens")
 	IMPL=Rule(_impl,name="Implication")
@@ -227,25 +326,34 @@ class Rules:
 
 class Operators:
 	NOPERATOR=Operator("def","",[],syntax="{0}",truthEvaluation=(lambda args:args))
-	AND=Operator("and","&",[Rules.SYM],truthEvaluation=(lambda a,b:a&b))
-	OR=Operator("or","|",[Rules.SYM,Rules.DS,Rules.RIMPL],truthEvaluation=(lambda a,b:a|b))
-	NOT=Operator("not","~",[],syntax="!{0}",truthEvaluation=(lambda a:True^a))
+	ABSENT=AOperator("A","*",[])
+	AND=Operator("and","&",[Rules.SYM,Rules.DM,Rules.SIMP,Rules.ADD],truthEvaluation=(lambda a,b:a&b))
+	OR=Operator("or","|",[Rules.SYM,Rules.DS,Rules.RIMPL,Rules.DM],truthEvaluation=(lambda a,b:a|b))
+	NOT=Operator("not","~",[Rules.RDM],syntax="!{0}",truthEvaluation=(lambda a:True^a))
 	IMPLICATION=Operator("implication",">",[Rules.HS,Rules.MP,Rules.IMPL],truthEvaluation=(lambda a,b:a<=b))
-	EQUIVILANCE=Operator("equivilance","=",[Rules.SYM],truthEvaluation=(lambda a,b:a==b))
+	EQUIVILENCE=Operator("equivalence","=",[Rules.SYM],truthEvaluation=(lambda a,b:a==b))
 
-if __name__=="__main__":
+def _test():
+	global StatementParenthesis
 	StatementParenthesis=False
-	x=Inspecific.X
-	y=Inspecific.Y
-	b=Statement(x,"B")
+	x=Insp.X
+	y=Insp.Y
 	a=Statement(x,"A")
+	b=Statement(x,"B")
 	c=Statement(x,"C")
 	d=Statement(x,"D")
-	conc=d
-	p=Pool([a>b,b>c,c>d,a])
+	conc=c
+	p=Pool([(~a|b)>c,a],conc)
 	p.completePool()
-	hal=p.checkForConclusion(conc)
-	print(p.pool)
-	print("\n\n".join([str(st) for st in p.iFullPool()]))
-	print(hal.parentLayers,hal,hal.parents)
-	print(p.proof(conc)[-2].parents)
+	print(((Insp.Xs>b)>c)==(((b*a).sym)>c))
+	print(Operators.ABSENT==Operators.AND)
+	print(Rules._addOrM(a,p)((~(a|b))|c))
+	hal=p.checkForConclusion()
+	print(p.contradictions)
+	print(p.pool,hal)
+	if hal==None:return()
+	propformat=lambda x:f"P{x[0]}) {x[1]} "+("" if x[2]==None else f"({', '.join([f'P{j}' for j in x[2][0]])} | {x[2][1].name})")
+	print("\n".join(map(propformat,p.proof()))+f"\nC) {conc}")
+
+if __name__=="__main__":
+	_test()
